0c7eefe8475791ac39229f1983e52ae4f1461223
Shubham121s <sp6063512@gmail.com>
2024-09-04 00:35:33 +0530

    change

--- src/components/ProfileUi.jsx
+++ src/components/ProfileUi.jsx
@@ -19,7 +19,7 @@ export default function Component() {
         return (
           <div className="text-[#8e8e93] space-y-4 overflow-y-auto max-h-48 pr-4 text-sm">
             <p>Hello! I'm Dave, your sales rep here from Salesforce. I've been working at this awesome company for 3 years now.</p>
-            <p>I was born and raised in Albany, NY & have been living in Santa Carla for the past 10 years with my wife Tiffany and my 4-year-old twin daughters, Emma and Ella. Both of them are just starting school, so my calendar is usually blocked between 9-10 AM.</p>
+            <p>I was born and raised in Albany, NY & have been living in Santa Carla for the past 10 years with my wife Tiffany and my 4-year-old twin daughters, Emma and Ella. Both of them are just starting school, so my calendar is usually blocked between 9-10 AM.This is a...</p>
           </div>
         );
       case 'experiences':
